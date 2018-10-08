4db126523a47b9f4a7572cbab537ef512e36c553
Zul-abdin <zulabdin4463@gmail.com>
2018-10-08 14:18:48 -0400

    BuyingApples.java added

---
+++ src/BuyingApples.java
@@ -0,0 +1,28 @@
+/*
+ * Displays the result of how much a certain number of pounds of apple cost.
+ */
+public class BuyingApples {
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter cost per LB: ");
+        double costLB = IO.readDouble();
+
+        System.out.print("Enter number of pounds: ");
+        double numberOfLB = IO.readDouble();
+
+        if ( costLB < 0 || numberOfLB < 0 ){
+
+            IO.reportBadInput();
+
+        } else {
+
+            double totalCost = numberOfLB * costLB;
+
+            IO.outputDoubleAnswer(totalCost);
+
+        }
+
+    }
+
+}
