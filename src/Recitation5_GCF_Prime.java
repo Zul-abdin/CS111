6c9fe60f49731659c7315719c1da039a0842e3d6
Zul-abdin <zulabdin4463@gmail.com>
2018-10-11 11:16:27 -0400

    Question from Recitation 5 Added
    Included Methods:
    Prime
    GCF
    Simplify Fraction

---
+++ src/Recitation5_GCF_Prime.java
@@ -0,0 +1,72 @@
+public class Recitation5_GCF_Prime {
+
+    public static  int GCF (int x, int y){
+
+        int GCF;   // Greatest Common Factor
+        int GPGCF; // Greatest Possible GCF
+
+        if ( x < y){
+
+            int temp = x;
+            x = y;
+            y = temp;
+
+        }
+
+        GPGCF = x - y;
+
+        if ( GPGCF > y){
+
+            GPGCF = y;
+
+        }
+
+        for (GCF = GPGCF; GCF > 1; GCF--){
+
+            if (x % GCF == 0 && y % GCF == 0){
+
+                return GCF;
+
+            }
+        }
+        return 1;
+    }
+
+    public static String SimplifyFraction(){
+
+        System.out.print("Enter Numerator: ");
+        int x = IO.readInt();
+
+        System.out.print("Enter Denominator: ");
+        int y = IO.readInt();
+
+        int GCF = GCF(x, y);
+
+        x /= GCF;
+        y /= GCF;
+
+        return x + "/" + y;
+    }
+
+    public static boolean isPrime (int x){
+
+        if (x < 2){
+            return false;
+        }
+
+        for(int i = 2; i < x; i++){
+
+            if ( x % i == 0){
+
+                return false;
+
+            }
+
+        }
+
+        return true;
+
+    }
+
+
+}
