89266d41f748bbd2b34db0a00f01620a1709385c
Zul-abdin <zulabdin4463@gmail.com>
2018-10-11 10:13:43 -0400

    Star Structures Added (Prints Star Art)

--- src/StarStructures.java
+++ src/StarStructures.java
@@ -29,4 +29,22 @@ public class StarStructures {
 
     }
 
+    public static void StarPyramid(int n){
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = i; j < n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
