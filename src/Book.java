ca7d6b8d41710428386e64c48c3b9ab334c58c6a
Zul-abdin <zulabdin4463@gmail.com>
2018-10-16 16:13:03 -0400

    Added Book.java (Opject class) and Bookcase.java Object Oriented Programming (OOP)

---
+++ src/Book.java
@@ -0,0 +1,47 @@
+public class Book {
+
+    // Instance Variables: Fields
+    /*
+     * Private - only accessible from inside the same class
+     */
+    private String title;
+    private String author;
+    private int pageNumber;
+
+
+    /*
+     * Constructor: a special method used to initialize the object and fill instance variables
+     * NO return type
+     * Same name as class name
+     */
+    public Book(String Booktitle){
+        title = Booktitle;
+        pageNumber = 0; //Assumes that the user has not read any pages
+    }
+
+    /*
+     * String Representation of an object
+     * Can be customized
+     * Notice: no Static modifier b/c this is a method devoted to an object
+     */
+    public String toString() {
+        return title + " [" + pageNumber + "] " + author;
+    }
+
+    //An instance method
+    public void turnPage() {
+        pageNumber += 1;
+    }
+
+    public void  multiPageTurn(int numberOfPages){
+        pageNumber += numberOfPages;
+    }
+
+    public Book (String Booktitle, String Bookauthor){
+
+        title = Booktitle;
+        author = Bookauthor;
+
+    }
+
+}
---
+++ src/Bookcase.java
@@ -0,0 +1,45 @@
+public class Bookcase {
+
+    public static void main(String[] args) {
+
+        // Instantiating an object
+        /*
+         * "book1" and "book2" are references.
+         */
+        Book book1 = new Book("Harry Potter Book 1");
+        Book book2 = new Book("Highlander");
+
+        /*
+         * the toString() method gets invoked by default
+         * Each object gets its own toString() method
+         *      - Each Object has its own unique method
+         */
+        System.out.println(book1);
+        System.out.println(book2);
+
+        //Invoke the instance method turnPage()
+        book1.turnPage();
+
+        // Turns 3 pages
+        // invoke turnPage() 3 times
+        book2.turnPage();
+        book2.turnPage();
+        book2.turnPage();
+
+        System.out.println(book1);
+        System.out.println(book2);
+
+        //Turn 10 pages in book 1
+        book1.multiPageTurn(10);
+
+        System.out.println(book1);
+        System.out.println(book2);
+
+        // Using a different constructor to instantiate the same type of object, Book
+        Book book3 = new Book("The Great Gatsby", "Some Random Dude");
+
+        System.out.println(book3);
+
+    }
+
+}
