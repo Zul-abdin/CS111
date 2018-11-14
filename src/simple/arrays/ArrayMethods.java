1e060b648e87ac9a8d2f19c4ed4359c7f57afd5b
Zul-abdin <zulabdin4463@gmail.com>
2018-11-14 14:07:41 -0500

    Added 2D Array methods to ArrayMethods.java from Study Group

--- src/simple/arrays/ArrayMethods.java
+++ src/simple/arrays/ArrayMethods.java
@@ -90,5 +90,50 @@ public class ArrayMethods {
             }
         }
     }
+    public static void print10TimesTable(){
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static int[][] rowSwap(int[][] arr, int row1, int row2){
+        int[] temp = arr[row1];
+        arr[row1] = arr[row2];
+        arr[row2] = temp;
+        return arr;
+    }
+    public static int[][] transpose(int[][] arr){
+        int[][] result = new int[arr[0].length][arr.length];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                result[j][i] = arr[i][j];
+            }
+        }
+        return result;
+    }
+    public static void print2DArrayOfInts(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void print1DArrayOfInts(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
+    }
+    public static String[][] StringCoordinates(int rows, int columns){
+        String[][] arr = new String[rows][columns];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                arr[i][j] = "(" + i + ", " + j + ") ";
+            }
+        }
+        return arr;
+    }
 
 }
